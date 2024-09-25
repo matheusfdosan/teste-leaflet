c8dfebf685591341e1f026cc8022d32afa613ff3
matheusfdosan <matheusfaustinoe20@gmail.com>
2024-09-25 09:12:52 -0300

    tudo funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 import React from "react"
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
+// import "leaflet/dist/leaflet.css"
 import "./App.css"
 
 function App() {
   const position = [-23.6865, -46.6234]
 
   return (
-    <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+    <MapContainer
+      center={position}
+      zoom={13}
+      scrollWheelZoom={true}
+      minZoom={8}
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       <Marker position={position}>
-        <Popup>
-          Vc está em São Paulo amigo
-        </Popup>
+        <Popup>Vc está em São Paulo amigo</Popup>
       </Marker>
     </MapContainer>
   )
