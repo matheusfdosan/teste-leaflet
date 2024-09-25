971e1bb22c6f3ef6469640582ecda954537feb42
matheusfdosan <matheusfaustinoe20@gmail.com>
2024-09-25 08:07:22 -0300

    teste com o leaflet

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
+import "./App.css"
+
+function App() {
+  const position = [-23.6865, -46.6234]
+
+  return (
+    <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          Vc está em São Paulo amigo
+        </Popup>
+      </Marker>
+    </MapContainer>
+  )
+}
+
+export default App
