5197e046e7e3f666ed5e3d0aa4df017d2c7159e4
matheusfdosan <matheusfaustinoe20@gmail.com>
2024-09-26 08:29:40 -0300

    mudei o icone do marcador

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from "react"
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
-// import "leaflet/dist/leaflet.css"
+import markerIcon from "./assets/vite.svg"
 import "./App.css"
 
 function App() {
   const position = [-23.6865, -46.6234]
+
+  // Definindo um novo ícone
+  const customIcon = new L.Icon({
+    iconUrl: markerIcon, // Caminho do ícone personalizado
+    iconSize: [25, 41], // Tamanho do ícone
+    iconAnchor: [12, 41], // Ponto de ancoragem (base do ícone)
+    popupAnchor: [1, -34], // Onde o popup será ancorado em relação ao ícone
+  })
 
   return (
     <MapContainer
@@ -17,8 +25,8 @@ function App() {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={position}>
-        <Popup>Vc está em São Paulo amigo</Popup>
+      <Marker position={position} icon={customIcon}>
+        <Popup>Vc está aqui amigo. Bem aqui!</Popup>
       </Marker>
     </MapContainer>
   )
