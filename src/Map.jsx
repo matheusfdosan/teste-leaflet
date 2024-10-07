0340e47b1de19cdbad2e05c47535a439ad360eb4
matheusfdosan <matheusfaustinoe20@gmail.com>
2024-10-06 23:35:26 -0300

    icon added

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react"
 import { MapContainer, TileLayer, Marker, Tooltip } from "react-leaflet"
+import markerIcon from "./assets/vite.svg"
+import location from "./utils/location.json"
 import "leaflet/dist/leaflet.css"
 import "./App.css"
-import location from "./utils/location.json"
 
 const LeafletMap = () => {
   const [locationData, setLocationData] = useState([])
@@ -10,6 +11,13 @@ const LeafletMap = () => {
   useEffect(() => {
     setLocationData(location)
   }, [])
+
+  const customIcon = new L.Icon({
+    iconUrl: markerIcon,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+  })
 
   return (
     <MapContainer
@@ -25,6 +33,7 @@ const LeafletMap = () => {
       {locationData.map((data, index) => (
         <Marker
           key={data.id || index}
+          icon={customIcon}
           position={[data.location.lat, data.location.long]}
         >
           <Tooltip>
